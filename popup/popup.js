7a201aee5328e21d09a5cd13ca43956f31415af6
Ebrahim Salehipanah <e.salehipanah@gmail.com>
2022-04-01 17:02:21 +0430

    fix

---
+++ popup/popup.js
@@ -0,0 +1,24 @@
+// TODO: add vue & npm
+// TODO: add localization
+// TODO: add lang selection
+const countryContainer = document.querySelector(".country");
+const loadingContainer = document.querySelector(".loading-container");
+var requestOptions = {
+  method: "GET",
+};
+
+loadingContainer.style.visibility = "visible";
+const API_KEY = "5e62e6bc995f44cfa39e26096d2473a3";
+fetch(`https://api.geoapify.com/v1/ipinfo?&apiKey=${API_KEY}`, requestOptions)
+  .then((response) => response.json())
+  .then(({ city, continent, country }) => {
+      console.log(getNameLocale(country));
+      countryContainer.innerHTML = getNameLocale(country)
+      loadingContainer.style.visibility = "hidden";
+  })
+  .catch((error) => console.log("error", error));
+
+
+  function getNameLocale({names}){
+      return names["fa"]
+  }
